a3bfcb54d9c4c0da64e5e8113deaa7e08f340aaf
Chau <xzhu000@citymail.cuny.edu>
2018-03-01 20:47:56 -0500

    ..

--- p1/test.c
+++ p1/test.c
@@ -6,76 +6,73 @@ struct listnode { struct listnode * next;
 struct listnode * sort( struct listnode * list) {
 	struct listnode * tmp;
 	unsigned int i, a;
-	struct listnode * bucket_begin[65536];
+	struct listnode * bucket_begin[65536];  //create buckets
 	struct listnode * bucket_end[65536];
-	for (i = 0; i<65536; i++) {
-		bucket_begin[i]=NULL;
-		bucket_end[i]=NULL;
-	}
-	tmp = list;
-	while (list != NULL)
+	for (i = 0; i<65536; i++)              //initialize buckets
 	{
-		list=list->next;
-		tmp->next=NULL;
+		bucket_begin[i]=NULL;
+		bucket_end[i]=NULL;	
+	}
+	for (tmp = list; tmp != NULL; tmp = list) //move number from list to corresponding bucket
+	{
+		list = list->next;
+		tmp->next = NULL;
 		a = tmp->key;
 		i = a&0xFFFF;
 		if (bucket_begin[i] == NULL)
 		{
 			bucket_begin[i] = tmp;
 			bucket_end[i] = tmp;
-		} else {
+		} else 
+		{
 			bucket_end[i]->next = tmp;
 			bucket_end[i] = tmp;
 		}
-		tmp = list;
 	}
-	i = 0;
-	list = bucket_begin[i];
+	// find the first non-empty bucket 
+	for (i = 0, list = bucket_begin[0]; list == NULL; i++, list = bucket_begin[i])
+		;
 	tmp = bucket_end[i];
-	i++;
-	while (i < 65536)
+	for (++i; i < 65536; i++)              //distribute numbers from buckets back to the list
 	{
 		if (bucket_begin[i] != NULL)
 		{
 			tmp->next = bucket_begin[i];
 			tmp = bucket_end[i];
 		}
-		i++;
 	}
-	// NEXT ROUND //
-	for (i = 0; i<65536; i++) {
-	bucket_begin[i]=NULL;
-	bucket_end[i]=NULL;
-	}	
-	tmp = list;
-	while (list != NULL)
+	/* NEXT ROUND */
+	for (i = 0; i<65536; i++) 
 	{
-		list=list->next;
-		tmp->next=NULL;
+		bucket_begin[i]=NULL;
+		bucket_end[i]=NULL;
+	}	
+	for (tmp = list; tmp != NULL; tmp = list)
+	{
+		list = list->next;
+		tmp->next = NULL;
 		a = tmp->key;
 		i = (a>>16)&0xFFFF;
 		if (bucket_begin[i] == NULL)
 		{
 			bucket_begin[i] = tmp;
 			bucket_end[i] = tmp;
-		} else {
+		} else 
+		{
 			bucket_end[i]->next = tmp;
 			bucket_end[i] = tmp;
 		}
-		tmp = list;
 	}
-	i=0;
-	list = bucket_begin[i];
+	for (i = 0, list = bucket_begin[0]; list == NULL; i++, list = bucket_begin[i])
+		;
 	tmp = bucket_end[i];
-	i++;
-	while (i < 65536)
+	for (++i; i < 65536; i++)
 	{
 		if (bucket_begin[i] != NULL)
 		{
 			tmp->next = bucket_begin[i];
 			tmp = bucket_end[i];
 		}
-		i++;
 	}	
 	return list;
 }
@@ -85,19 +82,17 @@ int main(void)
    unsigned int i; 
    struct listnode *list, *tmp;
    struct listnode space[10000000];
-   for( i=0; i< 10000000; i++ )
+   for( i=0; i<= 10000000; i++ )
       (space[i]).key  = 2*((139*i)%10000000);   
-   for( i=0; i< 10000000 -1; i++ )
+   for( i=0; i<= 10000000 -1; i++ )
      (space[i]).next = &(space[i+1]);
    (space[10000000 -1]).next = NULL;
    printf(" Finished preparing list\n"); fflush(stdout); 
    list = &(space[0]);
-	list = sort(list);
-
    for( tmp = list, i=0; tmp != NULL; tmp = tmp->next, i++)
 	; 
-   printf(" before sort: %d list items.\n", i); fflush( stdout); 
-
+   printf(" before sort: %d list items.\n", i); fflush( stdout);	
+   list = sort(list);
    for( tmp = list, i=0; tmp != NULL; tmp = tmp->next, i++)
      ;
    printf(" after  sort: %d list items.\n", i); fflush( stdout); 
@@ -108,6 +103,7 @@ int main(void)
        list = list-> next;
      }
    printf(" all sorted\n"); fflush(stdout);
+
 }
   
 
